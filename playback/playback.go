ee42d0d5c2e94fb3804dee7feca24603ca163164
Nick Ryberg <nryberg@gmail.com>
2015-09-12 22:16:15 -0500

    Finally representing in the template
    But still not quite figuring out who gets what.

--- playback/playback.go
+++ playback/playback.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
 	"github.com/boltdb/bolt"
 	"html/template"
 	"log"
@@ -30,6 +29,16 @@ type Track struct {
 
 func main() {
 	fs := http.FileServer(http.Dir("static"))
+	//http.Handle("/", fs)
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.HandleFunc("/tracks", serveTracks)
+	http.HandleFunc("/", serveTemplate)
+
+	log.Println("Listening...")
+	http.ListenAndServe(":3000", nil)
+}
+
+func fetchTracks(limit int) (Data, error) {
 	databasePath := os.Getenv("TRACKSDB")
 	log.Println("Database Path: ", databasePath)
 	db, err := bolt.Open(databasePath, 0600, nil)
@@ -38,29 +47,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	data, err := fetchTracks(db, 5)
-	log.Println(data.Timestamp, data.StationID)
-
-	//http.Handle("/", fs)
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-	http.HandleFunc("/", serveTemplate)
-
-	log.Println("Listening...")
-	http.ListenAndServe(":3000", nil)
-}
-
-func openDatabase(path string) (bolt.DB, error) {
-	db, err := bolt.Open(path, 0600, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-	return *db, err
-}
-
-func fetchTracks(db *bolt.DB, limit int) (Data, error) {
 	var data Data
-	err := errors.New("What happened?")
 	err = db.View(func(tx *bolt.Tx) error {
 
 		b := tx.Bucket([]byte("tracks"))
@@ -80,8 +67,44 @@ func fetchTracks(db *bolt.DB, limit int) (Data, error) {
 		return nil
 	})
 
+	log.Println(data.Timestamp, data.StationID)
 	return data, err
 }
+
+func openDatabase(path string) (bolt.DB, error) {
+	db, err := bolt.Open(path, 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	return *db, err
+}
+
+func serveTracks(w http.ResponseWriter, r *http.Request) {
+	lp := path.Join("templates", "layout.html")
+	fp := path.Join("templates", "tracks.html")
+
+	tmpl, err := template.ParseFiles(lp, fp)
+	if err != nil {
+		// Log the detailed error
+		log.Println(err.Error())
+		// Return a generic "Internal Server Error" message
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+	data, err := fetchTracks(1)
+	if err != nil {
+		// Log the detailed error
+		log.Println(err.Error())
+	}
+	log.Println("From Serve Tracks: ", data.StationID)
+	//if err := tmpl.ExecuteTemplate(w, "layout", data); err != nil {
+	if err := tmpl.ExecuteTemplate(w, "layout", data); err != nil {
+		log.Println(err.Error())
+		http.Error(w, http.StatusText(500), 500)
+	}
+}
+
 func serveTemplate(w http.ResponseWriter, r *http.Request) {
 	lp := path.Join("templates", "layout.html")
 	fp := path.Join("templates", r.URL.Path)
