ec7f5622934589e756499fcf8b58a41a0f62cf44
Nick Ryberg <nryberg@gmail.com>
2015-10-12 21:38:07 -0500

    Dump Data now converts to raw pieces only
    TimeID, StationID, ArtistID, SongID and a handy line number

--- process/dump_data.go
+++ process/dump_data.go
@@ -10,7 +10,7 @@ import (
 	//"io"
 	//"encoding/csv"
 	//"io/ioutil"
-	//"strconv"
+	"strconv"
 	"time"
 )
 
@@ -31,8 +31,15 @@ type Track struct {
 	Title    string `json:"trackTitle"`
 }
 
+type Entry struct {
+	TimeID    int64 `json:"timeID"`
+	StationID int64 `json:"stationID"`
+	ArtistID  int64 `json:"artistID"`
+	SongID    int64 `json:"songID"`
+}
+
 func main() {
-	build_test_bed(10)
+	build_test_bed(5)
 	dump_test_bed()
 }
 
@@ -42,21 +49,32 @@ func dump_test_bed() {
 		log.Fatal("Failure Opening database: ", err)
 	}
 	defer db.Close()
-	var data Data
-	fmt.Println("Line, Time, Artist, ArtistID, SongID, Title, Station, TimeUnix")
+	// var data Data
+	var entry Entry
+	fmt.Println("Line, TimeID, StationID, ArtistID, SongID")
 	err = db.View(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte("test"))
 		c := b.Cursor()
 		i := 0
 		for k, v := c.First(); k != nil; k, v = c.Next() {
 			if k != nil {
-				_ = json.Unmarshal(v, &data)
-				timed, _ := time.Parse(time.RFC3339, data.Timestamp)
-				time_number := timed.Unix()
-				for _, track := range data.Tracks {
-					fmt.Printf("%d,%s,%s,%d,%d,%q,%s,%d\n", i, k, track.Artist, track.ArtistID, track.SongID, track.Title, data.StationID, time_number)
-				}
+				_ = json.Unmarshal(v, &entry)
+				fmt.Printf("%d,%d,%d,%d,%d\n", i, entry.TimeID,
+					entry.StationID, entry.ArtistID, entry.SongID)
+				/*
+					timed, _ := time.Parse("2006-01-02T15:04:05-07:00", string(k[:]))
+					time_number = timed.Unix()
+					for _, track := range data.Tracks {
+						//fmt.Printf("%d,%s,%s,%d,%d,%q,%s,%d\n", i, k, track.Artist, track.ArtistID, track.SongID, track.Title, data.StationID, time_number)
+						//	fmt.Printf("%d,%s,%s,%d,%d,%q,%s,%d\n",
+						//		i, k, track.Artist, track.ArtistID, track.SongID,
+						//			track.Title, data.StationID, time_number)
+						fmt.Printf("%d,%s,%s,%d,%s\n",
+							i, k, track.Artist, time_number, timed)
+					}
+				*/
 				i += 1
+
 			}
 		}
 		return nil
@@ -93,6 +111,7 @@ func build_test_bed(limited int) {
 
 	db, err = openDB_ReadWrite()
 	defer db.Close()
+	var entry Entry
 	err = db.Update(func(tx *bolt.Tx) error {
 		_ = tx.DeleteBucket([]byte("test"))
 		b, err := tx.CreateBucketIfNotExists([]byte("test"))
@@ -100,9 +119,22 @@ func build_test_bed(limited int) {
 			log.Fatal("Failure : ", err)
 		}
 		for k := range entries {
-			err = b.Put([]byte(k), entries[k])
-			if err != nil {
-				log.Println("nope no can do: ", err)
+
+			_ = json.Unmarshal(entries[k], &data)
+			timed, _ := time.Parse("2006-01-02T15:04:05-07:00", data.Timestamp)
+			for _, track := range data.Tracks {
+
+				entry.TimeID = timed.Unix()
+				entry.StationID, err = strconv.ParseInt(data.StationID, 10, 64)
+				entry.ArtistID = track.ArtistID
+				entry.SongID = track.SongID
+
+				enc, err := json.Marshal(entry)
+				nextkey, _ := b.NextSequence()
+				err = b.Put(uint64_to_byte(nextkey), enc)
+				if err != nil {
+					log.Println("nope no can do: ", err)
+				}
 			}
 		}
 		return nil
@@ -113,6 +145,13 @@ func build_test_bed(limited int) {
 func int64_to_byte(number int64) []byte {
 	buf := make([]byte, binary.MaxVarintLen64)
 	_ = binary.PutVarint(buf, number)
+	return buf
+
+}
+
+func uint64_to_byte(number uint64) []byte {
+	buf := make([]byte, binary.MaxVarintLen64)
+	_ = binary.PutUvarint(buf, number)
 	return buf
 
 }
