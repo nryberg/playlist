be29dabc22d25e24c7535e588a2046e5780d68f5
Nick Ryberg <nryberg@gmail.com>
2015-11-05 07:25:52 -0600

    Moves artists in direction of just artist data

---
+++ notes/2015_11_04_At_the_point.md
@@ -0,0 +1,7 @@
+At the point of entry
+
+Right now I'm doing batch operations and chafing at the return times.  No wonder - it's  lot of data, and you should be doing testing on small quantities rather than the full Monty.  
+
+Whether or not this would be solved by going to Postgresql is debatable - you still are going to have to pay the price in migration. You're still going to have now manage security at a higher level.  At the end of this rainbow however is the fact that Tableau will hook directly up to it - no fuss no muss.
+
+On one level you're pulling back one of the chips of innovation - good!  On the other hand, and there always is another hand - you're going to incur the penalties of learning the new tech - not a bad thing since a) it's SQL at the core, and b) you need to keep learning. 
--- process/process_artists.go
+++ process/process_artists.go
@@ -63,8 +63,8 @@ func main() {
 	buildabucket(db, "artists")
 	buildabucket(db, "artist_name_id")
 
-	data, err := FetchAllTracks(db, "stations")
-	log.Println("Length: ", len(data))
+	artists, err := FetchAllTrackArtists(db, "tracks")
+	log.Println("Entry Count: ", len(data))
 	var artist Artist
 
 	data_count := 0
@@ -131,38 +131,76 @@ func writeString(key string, value string, bucket_name string, db *bolt.DB) erro
 	return err
 }
 
-func FetchTracks(db *bolt.DB, bucket_name string, limit int) ([]Data, error) {
-	var data []Data
+func FetchTracks(db *bolt.DB, bucket_name string, limit int) ([]Artists, error) {
 	var datum Data
+	var artists []Artist
+	var artist Artist
 	err := db.View(func(tx *bolt.Tx) error {
 
 		b := tx.Bucket([]byte("tracks"))
 		c := b.Cursor()
+		//for k, v := c.First(); k != nil; k, v = c.Next() {
+
 		k, v := c.First()
 		for i := 0; i <= limit; i++ {
-			//for k, v := c.First(); k != nil; k, v = c.Next() {
 			if k != nil {
 				err := json.Unmarshal(v, &datum)
 				if err != nil {
 					log.Fatal(err)
 				}
-				key := string(k[:])
-				datum.Timestamp = key
-				data = append(data, datum)
-			}
+				for _, a := range datum.Tracks {
+					artist.Name = a.Artist
+					artist.ArtistID = a.ArtistID
+					artists = append(artists, artist)
+				} // Pull Artist info
+			} // make sure k isn't nil
 			k, v = c.Next()
+		} // iterate through limited results
+
+		return nil
+	})
+
+	log.Println("artist count: ", len(artists))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return artists, err
+
+}
+
+func FetchAllTrackArtists(db *bolt.DB, bucket_name string) ([]Artist, error) {
+	var datum Data
+	var artists []Artist
+	var artist Artist
+	err := db.View(func(tx *bolt.Tx) error {
+
+		b := tx.Bucket([]byte("tracks"))
+		c := b.Cursor()
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			if k != nil {
+				err := json.Unmarshal(v, &datum)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				for _, a := range datum.Tracks {
+					artist.Name = a.Artist
+					artist.ArtistID = a.ArtistID
+					artists = append(artists, artist)
+
+				}
+			}
 		}
 
 		return nil
 	})
 
-	log.Println(datum.Timestamp, datum.StationID)
+	log.Println("artist count: ", len(artists))
 	if err != nil {
 		log.Fatal(err)
 	}
-	return data, err
+	return artists, err
 }
-
 func FetchAllTracks(db *bolt.DB, bucket_name string) ([]Data, error) {
 	var data []Data
 	var datum Data
